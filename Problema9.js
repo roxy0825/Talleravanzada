968dc3079e7eb8af85967a99a05e8f4e231db29f
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-27 20:35:16 -0500

    2

---
+++ Problema9.js
@@ -0,0 +1,19 @@
+function crearEstudiante(nombre,planeta,edad,estatura,clasificarEstudiante){
+    setTimeout(function(){
+        let estudiante={
+            nombre:nombre,
+            edad:edad,
+            planeta:planeta,
+            estatura:estatura
+        }
+        clasificarEstudiante(estudiante.edad)
+
+    },1000)
+}
+crearEstudiante("pedro","martes",18,1.86,function(edad){
+    if (edad<15){
+        console.log("Su edad es " + edad + " tu clase sera manejo de la fuerza")
+    }else{
+        console.log("Su edad es " +edad+ " tu clase sera uso de sable")
+    }
+})
